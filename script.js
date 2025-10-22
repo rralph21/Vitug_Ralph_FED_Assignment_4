637a113d967e48295c6a0b5da993704b18a45b79
Ralph.Vitug <rralph@rrc.ca>
2025-10-21 20:34:21 -0500

    initial .js form

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
 document.addEventListener("DOMContentLoaded", function () {
   const form = document.querySelector("form");
-
-  function val(id) { return document.getElementById(id).value.trim(); }
-  function setErr(id, msg) { document.getElementById(id).textContent = msg; }
-  function clearAllErrors() {
-    document.querySelectorAll(".error-text").forEach(el => el.textContent = "");
-  } 
 });
